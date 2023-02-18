0156e9385b19faf772da2d40b09fa9f969c84c42
Israr Ahmad <56542750+shari45@users.noreply.github.com>
2023-02-18 19:48:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
+from flask_ngrok import run_with_ngrok
 from flask import Flask, render_template, request
-from flas_ngrok import run_with_ngrok
+
 import torch
 from diffusers import StableDiffusionPipeline
 
 import base64
 from io import BytesIO
 
-pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", revision='fp16", torch_dtype=torch.float16
-
-
+# Load model
+pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", revision="fp16", torch_dtype=torch.float16)
 pipe.to("cuda")
 
-
-app=Flask(__name__)
+# Start flask app and set to ngrok
+app = Flask(__name__)
 run_with_ngrok(app)
 
-
-@app.route("/")
+@app.route('/')
 def initial():
-	return render_template("index.html")
+  return render_template('index.html')
 
-@app.route("/submit-prompt, methods=["Post"])
+
+@app.route('/submit-prompt', methods=['POST'])
 def generate_image():
-	prompt = request.form("prompt-input")
-	image = pipe(prompt).image[0]
-	buffered = BytesIO()
-	image.save(buffered, format = "PNG")
-	img_str = base64.b64encode(buffered.getvalue())
-	img_str = "data:image/png;base64," +str(img_str)[2:-1]
-	return render_template("index.html", generated_image=img_str)
+  prompt = request.form['prompt-input']
+  print(f"Generating an image of {prompt}")
 
+  image = pipe(prompt).images[0]
+  print("Image generated! Converting image ...")
+  
+  buffered = BytesIO()
+  image.save(buffered, format="PNG")
+  img_str = base64.b64encode(buffered.getvalue())
+  img_str = "data:image/png;base64," + str(img_str)[2:-1]
+
+  print("Sending image ...")
+  return render_template('index.html', generated_image=img_str)
+
+
+if __name__ == '__main__':
+    app.run()
 
 
